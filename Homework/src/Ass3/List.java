baed3d8835438592455ea71bacfbfab4dac49be3
Risu <11910206@mail.sustech.edu.cn>
2021-04-09 19:40:21 +0800

    add some changes

--- Homework/src/Ass3/List.java
+++ Homework/src/Ass3/List.java
@@ -37,9 +37,100 @@ public class List {
     // sort the list ascending. Any sorting algorithm is fine.
     // attribute sorted should be changed to 1
     public void sort() {
-
+        sortList(headListNode);
     }
 
+    // use merge sort to solve the sorting problem
+    // run in O(NlgN) and space cost is O(lgN)
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // first step recursively cut the list
+        ListNode newHead = cutToHalf(head);
+        ListNode sortedFirstHead = sortList(head);
+        ListNode sortedSecondHead = sortList(newHead);
+
+        // second step sort and merge them together
+        return merge(sortedFirstHead, sortedSecondHead);
+    }
+
+    // use fast-slow pointer to find the middle point of a list
+    // return the head of the second list
+    public ListNode cutToHalf(ListNode head) {
+        // if (head == null) {
+        // throw new Exception(">> cutToHalf has null input");
+        // }
+        if (head.next == null) {
+            return head;
+        }
+
+        ListNode slowPoint = head;
+        ListNode fastPoint = head.next;
+
+        // length is even, this is the point at length / 2
+        // length is odd, this is the point at (length + 1) / 2
+        while (true) {
+            if (fastPoint == null || fastPoint.next == null) {
+                ListNode secondHead = slowPoint.next;
+                slowPoint.next = null;
+                return secondHead;
+            }
+
+            fastPoint = fastPoint.next.next;
+            slowPoint = slowPoint.next;
+        }
+    }
+
+    // merge the sorted sublist together
+    // return the head of the new list
+    public ListNode merge(ListNode head, ListNode secondHead) {
+        // if (head == null || secondHead == null) {
+        // throw new Exception(">> merge has null input");
+        // }
+
+        ListNode firstPoint = head;
+        ListNode secondPoint = secondHead;
+        ListNode newHead = null;
+        ListNode cur = null;
+
+        while (true) {
+            if (firstPoint == null) {
+                assert cur != null;
+                cur.next = secondPoint; // the left nodes of the next sublist are sorted
+                return newHead;
+            } else if (secondPoint == null) {
+                assert cur != null;
+                cur.next = firstPoint;
+                return newHead;
+            }
+
+            // using merge sort
+            // sort and merge the two list together
+            if (firstPoint.val <= secondPoint.val) {
+                if (newHead == null) {
+                    newHead = firstPoint;
+                    cur = firstPoint;
+                } else {
+                    cur.next = firstPoint;
+                    cur = cur.next;
+                }
+                // firstPoint.next = null;
+                firstPoint = firstPoint.next;
+            } else {
+                if (newHead == null) {
+                    newHead = secondPoint;
+                    cur = secondPoint;
+                } else {
+                    cur.next = secondPoint;
+                    cur = cur.next;
+                }
+                // secondPoint.next = null;
+                secondPoint = secondPoint.next;
+            }
+        }
+    }
 
     // reverse the order of nodes of list
     // attribute “sorted” should be changed if the list is sorted before
